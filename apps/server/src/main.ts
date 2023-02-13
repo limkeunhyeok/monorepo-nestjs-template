49e15809f86a89ac097452899ebfb0f0e7d97a6b
limkeunhyeok <gorloom6425@naver.com>
2023-02-13 22:37:04 +0900

    feat: add dto validation pipe

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,4 +1,4 @@
-import { AllExceptionsFilter } from '@common/core';
+import { AllExceptionsFilter, DtoValidationPipe } from '@common/core';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(new DtoValidationPipe());
 
   await app.listen(3000);
 }
---
+++ libs/core/src/pipes/dto-validation.pipe.ts
@@ -0,0 +1,42 @@
+import { classToClass } from '@nestjs/class-transformer';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class DtoValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToInstance(metatype, value);
+    const converted = classToClass(object);
+
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      throw new SchemaException({
+        name: 'SchemaValidationError',
+        message: String(errors),
+      });
+    }
+
+    return converted;
+  }
+
+  private toValidate(metatype): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
+
+export class SchemaException extends BadRequestException {
+  constructor(error: Error) {
+    super(error);
+  }
+}
