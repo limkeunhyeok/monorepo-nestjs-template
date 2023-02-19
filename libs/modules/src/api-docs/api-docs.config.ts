b266f733c83fb99d8bc44230d528dc4a37903013
limkeunhyeok <gorloom6425@naver.com>
2023-02-19 21:38:38 +0900

    feat: add api-docs module

---
+++ libs/modules/src/api-docs/api-docs.config.ts
@@ -0,0 +1,20 @@
+import { DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
+
+export interface ApiDocsOptions {
+  title: string;
+  description: string;
+  version: string;
+}
+
+export const getSwaggerConfig = ({
+  title,
+  description,
+  version,
+}: ApiDocsOptions): Omit<OpenAPIObject, 'paths'> => {
+  const config = new DocumentBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setVersion(version)
+    .build();
+  return config;
+};
--- libs/modules/src/index.ts
+++ libs/modules/src/index.ts
@@ -1,1 +1,2 @@
+export * from './api-docs';
 export * from './lib';
